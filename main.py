ce9fb948ce0a298637b1d8fa3d20e2824ab5db37
Devansh Bhardwaj <dbhardwaj123411@gmail.com>
2026-05-29 18:09:16 +0530

    input and output arguments added and tested

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import pytesseract
 import argparse
 from PIL import Image
 from preprocessor import preprocess
+from pathlib import Path
+import sys
 
 try:
     parser = argparse.ArgumentParser(prog="tex", description="Extracts text from image")
 
     parser.add_argument("-i", "--image", help="Takes path to an image file")
     parser.add_argument("-l", "--lang", help="Takes language of text", default="eng")
-    parser.add_argument("--preprocess", action="store_true")
+    parser.add_argument(
+        "--preprocess", action="store_true", help="Clears image before conversion"
+    )
+    parser.add_argument("-o", "--output", help="Takes the output path")
+    parser.add_argument("--print", action="store_true")
 
     args = parser.parse_args()
 
@@ -19,8 +25,23 @@ try:
     text = pytesseract.image_to_string(image, args.lang)
     if text == "":
         print(f"No text found in {args.image}")
-    else:
+        sys.exit(0)
+
+    if args.print:
         print(text)
+
+    if args.output:
+        with open(args.output, "w", encoding="utf-8") as f:
+            f.write(text)
+        print(f"Text saved to {args.output}")
+
+    else:
+        path = args.image
+        parent = Path(path).parent
+        newPath = parent / "output.txt"
+        with open(newPath, "w", encoding="utf-8") as f:
+            f.write(text)
+        print(f"Text saved to {newPath}")
 
 
 except Exception as e:
