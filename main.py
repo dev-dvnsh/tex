b4fe8e2eca9d0e5059f44b88d9aacaa7a898d642
Devansh Bhardwaj <dbhardwaj123411@gmail.com>
2026-05-29 17:07:59 +0530

    core functionality ready the -i image is getting converted to text and -l or --lang cannow take a language for the text in given image

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import pytesseract
 import argparse
 from PIL import Image
 
 try:
     parser = argparse.ArgumentParser(prog="tex", description="Extracts text from image")
 
-    parser.add_argument("-i", "--image")
+    parser.add_argument("-i", "--image", help="Takes path to an image file")
+    parser.add_argument("-l", "--lang", help="Takes language of text", default="eng")
 
     args = parser.parse_args()
-    imgData = Image.open(args.image)
-    print(imgData.size, imgData.format, imgData.mode)
+    text = pytesseract.image_to_string(args.image, args.lang)
+    if text == "":
+        print(f"No text found in {args.image}")
+    else:
+        print(text)
+
 
 except Exception as e:
     print(e)
